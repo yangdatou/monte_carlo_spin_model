ed6aa02e49942d929148762b2f0360f758e185b4
yangdatou <244140486@qq.com>
2020-09-06 16:35:22 -0500

    update

--- spin_model/spin_model.py
+++ spin_model/spin_model.py
@@ -1,12 +1,15 @@
 from .spin_site import IsingSpinSite
 
+
 class SpinModel(object):
     ''' Abstract base class for spin model that used to store the
     parameters of the system'''
     pass
 
+
 class IsingModel(SpinModel):
     ''' '''
+
     def __init__(self, interaction_strength=1.0):
         self.interaction_strength = interaction_strength
 
@@ -15,12 +18,14 @@ class IsingModel(SpinModel):
             interaction_strength=self.interaction_strength,
             config=config, index=index)
 
+
 class XYModel(SpinModel):
     def __init__(self):
         pass
 
     def get_site(self):
         pass
+
 
 class HubbardModel(SpinModel):
     def __init__(self):
--- spin_model/spin_site.py
+++ spin_model/spin_site.py
@@ -1,22 +1,24 @@
 from numpy.random import randint
 from numpy.random import random
 
+
 class SpinSite(object):
     ''' '''
     pass
 
+
 class IsingSpinSite(SpinSite):
-    def __init__(self, config = 0, interaction_strength = 1.0, index = 1):
+    def __init__(self, config=0, interaction_strength=1.0, index=1):
         self.interaction_strength = interaction_strength
         self.index = index
 
-        if config in [-1,1]:
+        if config in [-1, 1]:
             self.config = config
         else:
             RuntimeError("Wrong config value!")
 
     def set_config(self, config):
-        if config in [0,1]:
+        if config in [0, 1]:
             self.config = config
 
     def set_random_config(self):
