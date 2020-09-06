6e09e25146b4bec21abcacc2ca471800ec6cae79
yangdatou <244140486@qq.com>
2020-09-05 19:48:21 -0500

    update

---
+++ spin_model/spin_model.py
@@ -0,0 +1,23 @@
+class SpinModel(object):
+    pass
+
+class IsingModel(SpinModel):
+    def __init__(self):
+        pass
+
+    def get_site(self):
+        pass
+
+class XYModel(SpinModel):
+    def __init__(self):
+        pass
+
+    def get_site(self):
+        pass
+
+class HubbardModel(SpinModel):
+    def __init__(self):
+        pass
+
+    def get_site(self):
+        pass
